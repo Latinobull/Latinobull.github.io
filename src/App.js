9d89f35d04c10839ad19cceac23adc2768dfd454
Donnahue George <Donnahuegjr@gmail.com>
2021-04-02 09:55:24 -0400

    added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
 function App() {
-  return <div className="App"> Work in progress </div>;
+  return (
+    <div className="uk-align-center">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+function Header() {
+  return (
+    <header>
+      <h1 className="uk-align-center">Donnahue George</h1>
+    </header>
+  );
+}
+
+export default Header;
