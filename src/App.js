0ee227b88c47a7d3018146f8e86d33d7cb0187ef
Donnahue George <Donnahuegjr@gmail.com>
2021-04-03 19:42:42 -0400

    need to get off canvas to finish working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Profile from './components/Profile';
 import Links from './components/Links';
 import Project from './components/Project';
+import Offcanvas from './components/OffCanvas';
 function App() {
   return (
     <div className="uk-align-center">
@@ -12,6 +13,7 @@ function App() {
       <Links />
       <hr />
       <Project />
+      <Offcanvas />
     </div>
   );
 }
